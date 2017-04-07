f45b0bacc9197eabdf71f06239de637f54f83eee
Marco Moschettini <m.marmos@gmail.com>
2017-04-07 23:01:48 +0200

    Minor changes.

--- test/Receiver.ts
+++ test/Receiver.ts
@@ -1,7 +1,7 @@
 import {Peer} from '../src/lib/Peer';
 
 let peer = new Peer('PeerB', {
-    rendezvous: {host: 'localhost', port: 4321}
+    rendezvous: {host: 'api.cubbit.net', port: 4321}
 });
 
 peer.listen().then(() =>
